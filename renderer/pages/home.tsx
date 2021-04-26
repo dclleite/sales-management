e02f730476242581b476db0189d6b5be730c843d
dclleite <eduappsplay@gmail.com>
2021-04-25 22:43:25 -0300

    first commit

---
+++ renderer/pages/home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Head>
+        <title>Dashboard - Neoplast</title>
+      </Head>
+      <div>
+        <p>Hello world</p>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default Home;
