8f5957773b30ec7af898360b7133d9ea7c280938
Raul Costa <raulbrennersc@gmail.com>
2021-05-17 20:26:11 -0300

    add table definitions

--- db/migrations/20210509204226_init_migration.js
+++ db/migrations/20210509204226_init_migration.js
@@ -1,35 +1,76 @@
-
-exports.up = function(knex) {
+exports.up = function (knex) {
   return knex.schema
-    .createTable('clientes', function (table) {
-      table.increments('id').primary();
-      table.string('nome').notNullable();
+    .createTable('client', function (table) {
+      table.uuid('id').primary();
+      table.string('name').notNullable();
       table.string('cpfCnpj');
-      table.text('endereco');
-      table.string('telefone' );
+      table.string('phone');
+      table.string('email');
+      table.string('street');
+      table.string('streetNumber');
+      table.string('neighborhood');
+      table.string('cep');
+      table.string('city');
+      table.string('state');
     })
-    .createTable('produtos', function (table) {
-      table.increments('id').primary();
-      table.string('nome').notNullable();
-      table.string('unidade', 10).notNullable();
-      table.decimal('valor').notNullable();
-      table.string('descricao', 1000);
+    .createTable('supplyer', function (table) {
+      table.uuid('id').primary();
+      table.string('name').notNullable();
+      table.string('cpfCnpj');
+      table.string('stateRegistration');
+      table.string('phone');
+      table.string('email');
+      table.string('street');
+      table.string('streetNumber');
+      table.string('neighborhood');
+      table.string('cep');
+      table.string('city');
+      table.string('state');
     })
-    .createTable('precoProduto', function (table) {
-      table.increments('id').primary();
-      table.integer('clienteId').notNullable();
-      table.integer('produtoId').notNullable();
-      table.decimal('valor').notNullable();
+    .createTable('product', function (table) {
+      table.uuid('id').primary();
+      table.string('name').notNullable();
+      table.uuid('unitId').notNullable();
+      table.decimal('price').notNullable();
 
-      table.foreign('clienteId').references('id').inTable('clientes');
-      table.foreign('produtoId').references('id').inTable('produtos');
-    });
+      table.foreign('unitId').references('id').inTable('unit');
+    })
+    .createTable('productPrice', function (table) {
+      table.uuid('id').primary();
+      table.uuid('productId').notNullable();
+      table.uuid('clientId').notNullable();
+      table.decimal('price').notNullable();
+
+      table.foreign('clientId').references('id').inTable('client');
+      table.foreign('productId').references('id').inTable('product');
+    })
+    .createTable('order', function (table) {
+      table.uuid('id').primary();
+      table.uuid('clientId').notNullable();
+      table.date('orderDate').notNullable();
+      table.date('deliveryDate').notNullable();
+
+      table.foreign('clientId').references('id').inTable('client');
+    })
+    .createTable('orderProduct', function (table) {
+      table.uuid('id').primary();
+      table.uuid('orderId').notNullable();
+      table.uuid('productId').notNullable();
+      table.integer('quantity').notNullable();
+
+      table.foreign('orderId').references('id').inTable('order');
+      table.foreign('productId').references('id').inTable('product');
+    })
+    ;
 
 };
 
-exports.down = function(knex) {
+exports.down = function (knex) {
   return knex.schema
-    .dropTable("precoProduto")
-    .dropTable("produtos")
-    .dropTable("clientes");
+    .dropTable("client")
+    .dropTable("supplyer")
+    .dropTable("productPrice")
+    .dropTable("order")
+    .dropTable("orderProduct")
+    .dropTable("product");
 };
