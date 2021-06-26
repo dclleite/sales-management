d645496ead9b3e51c099290e20a4842dbeda64cb
Raul Costa <raulbrennersc@gmail.com>
2021-06-26 15:18:57 -0300

    improve custom select

--- renderer/components/CustomSelect/index.tsx
+++ renderer/components/CustomSelect/index.tsx
@@ -13,11 +13,15 @@ interface ISelectProps {
 }
 
 export function CustomSelect({ items, keyProp = 'id', titleProp = 'name', onChange, label, style = {}, value = '' }: ISelectProps) {
+  function getValue() {
+    const selected = items.find(item => item[keyProp] ?? item === value[keyProp] ?? value)
+    return selected[keyProp] ?? selected
+  }
   return (
     <div className={styles.custmSelectContainer} style={{ display: 'flex', flexDirection: 'column', ...style }}>
       <label className={styles.label} >{label}</label>
       <div className={styles.selectContainer}>
-        <Select value={value} className={styles.select} onChange={onChange} label="texto" disableUnderline>
+        <Select value={getValue()} className={styles.select} onChange={onChange} label="texto" disableUnderline>
           {items.map(item => <MenuItem key={item[keyProp] ?? item} value={item}>{item[titleProp] ?? item}</MenuItem>)}
         </Select>
       </div>
